c2d3c57f5439b3add4f1ac2a77b17602f0ab13ba
tayrone <tayronetnm@gmail.com>
2018-11-15 10:39:58 -0300

    Two-way data bind

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -7,12 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  informacoes ={
+    nome: '',
+    idade: '25',
+    email: 't@t.com',
+    telefone: '',
+    skype: '',
+    cor: ''
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  // SUBMISSAO DO FORMULARIO, TIPO O MYFORM COMO NGFORM TEM-SE ACESSO AOS METODOS DELE
+  // SUBMISSAO DO FORMULARIO, TIPANDO O MYFORM COMO NGFORM TEM-SE ACESSO AOS METODOS DELE
   onSubmit(myForm) {
     console.log(myForm);
   }

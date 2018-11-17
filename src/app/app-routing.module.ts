7be1f2680448343b07c550126d06b0ee04710416
tayrone <tayronetnm@gmail.com>
2018-11-17 15:16:14 -0300

    Separando rotas para cada módulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,34 +4,7 @@ import { CoursesListComponent } from './courses/courses-list.component';
 import { RouterGuardGuard } from './router-guard.guard';
 
 // AQUI SAO FEITAS AS CONFIGURACOES DAS ROTAS
-const routes: Routes = [
-// PARA EVITAR REPETICOES DE CURSOS AGRUPO TODOS OS ELEMENTOS DETRO DE UM ARRAY CHILD
-  {
-    // ROTA PRINCIPAL
-    path: 'cursos',
-    children: [
-        // ROTAS FLHAS ROTA QUE RECEBE VARIAVEL VEM por ultimo
-      {
-        path: ':nome',
-        component: CoursesListComponent,
-        canActivate: [RouterGuardGuard]
-      },
-      // {
-      //   path: 'atualizar',
-      //   component: CoursesListComponent
-      // },
-      // {
-      //   path: ':novo',
-      //   component: CoursesListComponent
-      // },
-    ]
-  },
-  // PASSANDO ROTA COM PARAMETRO DOIS PONTOS MAIS VARIAVEL
-  {
-    path: 'cursos/:nome',
-    component: CoursesListComponent
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

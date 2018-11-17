08f78a7d1c4151e218bfdb6e8344446f7e7277f9
tayrone <tayronetnm@gmail.com>
2018-11-17 15:08:30 -0300

    CanDeactivate - proteger saia de rota

--- src/app/router-guard.guard.ts
+++ src/app/router-guard.guard.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, CanDeactivate, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { CoursesListComponent } from './courses/courses-list.component';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouterGuardGuard implements CanActivate {
+export class RouterGuardGuard implements CanActivate, CanDeactivate<CoursesListComponent> {
   canActivate(
     // PROXIMA ROTA
     next: ActivatedRouteSnapshot,
@@ -14,4 +15,16 @@ export class RouterGuardGuard implements CanActivate {
     // FALSE NAO ENTRA
     return true;
   }
-}
+
+  // NAO DEIXA SAIR SE RETORNAR FAALSO
+  canDeactivate(
+    component: CoursesListComponent,
+    next: ActivatedRouteSnapshot,
+    // ESTADO DA ROTA
+    state: RouterStateSnapshot): boolean {
+    // FALSE NAO ENTRA
+    return true;
+  }
+
+  }
+
